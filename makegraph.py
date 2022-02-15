00addf7572515cf3b3fde3115dc88fb443e47285
Keita Ito <kito@ux-xu.com>
2022-02-15 12:22:29 +0900

    feat: Introduce FIR filter & get peaks

--- makegraph.py
+++ makegraph.py
@@ -2,32 +2,42 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
 
 # load log data
+name_id = "default_400_av2_thumb/thumb_"
+name_id = "thumb_"
 red = []
-with open("./red.log", "r") as f:
+with open("./" + name_id + "red.log", "r") as f:
     for r in f:
         red.append(int(r))
 
 ir = []
-with open("./ir.log", "r") as f:
+with open("./" + name_id + "ir.log", "r") as f:
     for r in f:
         ir.append(int(r))
 
+filter1 = signal.firwin(numtaps=401, cutoff=[0.4,4], fs=400, pass_zero=False)
+normal_ir = signal.lfilter(filter1, 1, ir)
+
+peaks, _ = signal.find_peaks(normal_ir)
+
+zero_ir = signal.filtfilt(filter1, 1, ir)
+
 # x-axis values
-x = np.arange(len(red))
+x = np.arange(len(ir))
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
 # RED LED
-ax.plot(x, red, c="red", label="RED LED")
-# IR LED (decrease values to show near RED LED)
-ax.plot(x, np.array(ir)-20000, c="orange", label="IR LED (shifted)")
+ax.plot(x, normal_ir, c="orange", label="FIR")
+ax.plot(peaks, normal_ir[peaks],linestyle='None', marker='o')
+ax.plot(x, ir, c="red", label="RAW")
 
 # modify limits
-ax.set_ylim(122000, 126000)
-# ax.set_xlim(400,600)
+# ax.set_ylim(153000, 190000)
+# ax.set_xlim(0,350)
 
 # show legend
 ax.legend(loc="best")
